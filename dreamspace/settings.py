421c285b53fcc361f75e0ad2e28a476c38f52817
Doboy <Doboy@Huans-MacBook-Pro.local>
2011-11-06 12:15:40 -0800

    s

--- dreamspace/settings.py
+++ dreamspace/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'sqlite3',
-        'NAME': '/Users/Doboy/Django/dreamspace/database/mydata.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/Doboy/Django/dreamspace/dreamspace/database/mydata.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'dreamspace.urls'
 
 TEMPLATE_DIRS = (
-    '/Users/Doboy/Django/dreamspace/templates',
+    '/Users/Doboy/Django/dreamspace/dreamspace/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
