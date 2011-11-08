b175f4c168621b45417ce6421b8c9388f2183f1c
Doboy <Doboy@Huans-MacBook-Pro.local>
2011-11-07 21:03:27 -0800

    fixed timedate field

--- dreamspace/blog/models.py
+++ dreamspace/blog/models.py
@@ -4,7 +4,7 @@ class Post( models.Model ):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
     public = models.BooleanField()
-    time = DateTimeField( auto_now_add=True )
+    time = models.DateTimeField( auto_now_add=True )
     user = models.CharField(max_length=30)
     
     def __unicode( self ):
